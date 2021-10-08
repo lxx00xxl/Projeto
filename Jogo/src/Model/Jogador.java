c284566cad5cedae1187fff6c6f9f66462d806d2
caiovalle2 <caiovalle1999@hotmail.com>
2021-10-08 11:42:44 -0300

    Update Jogador.java

--- Jogo/src/Model/Jogador.java
+++ Jogo/src/Model/Jogador.java
@@ -1,5 +1,33 @@
 package Model;
 
-public class Jogador {
 
+class Jogador{
+    private int exploradores[][] = new int[6][2];
+    private int pontos = 0;
+    private char cartas[] = new char[10];
+    char cor ;
+    public Jogador(int polo[], char cor){
+        this.cor = cor;
+        for (int i=0;i<6;i++){
+            exploradores[i][0] = polo[0];
+            exploradores[i][1] = polo[1];
+        }
+    }
+    public int[] getposicao(int ind){
+        return exploradores[ind];
+    }
+    void setposicao(int ind,int i,int j) {
+    	exploradores[ind][0] = j;
+    	exploradores[ind][1] = i;
+    	
+    }
+    public void somarpontos(int pontos){
+        this.pontos += pontos;
+    }
+    public int getpontos(){
+        return pontos;
+    }
+    char getcor() {
+    	return cor;
+    }
 }
