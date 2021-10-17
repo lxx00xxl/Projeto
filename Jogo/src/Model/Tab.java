b4a86b4e443d3a08dacd19b34bfc87e0fb49ad8e
caiovalle2 <caiovalle1999@hotmail.com>
2021-10-17 08:27:12 -0300

    Update Tab.java

--- Jogo/src/Model/Tab.java
+++ Jogo/src/Model/Tab.java
@@ -2,7 +2,7 @@ package Model;
 
 public class Tab {
 	String tab[][] = new String[15][20];
-
+	int metas[] = {0,0};
 	
 	public Tab(String[][] tab) {
 		for(int i = 0; i<15; i++){
@@ -25,6 +25,7 @@ public class Tab {
         char cor[] = {'A','V','R','L'}; 
         i = pos_ant[0];
         j = pos_ant[1];
+        ver_metas(reg, ind_jog);
         if(a.ver_pos(ind_jog, i,j)[1] == 0) {
         	tab[i][j] = "0";
         }
@@ -33,12 +34,27 @@ public class Tab {
         }
         i = a.getposicao(ind_jog,ind_exp)[0];
         j = a.getposicao(ind_jog,ind_exp)[1];
+        if(metas[0] == 0) {
+        	tab[8][2] = "M";
+        }
+        if(metas[1] == 0) {
+        	tab[8][8] = "M";
+        }
         tab[8][4] = "P";
 		tab[8][14] = "P";
         tab[i][j] = cor[ind_jog] + Integer.toString(a.ver_pos(ind_jog, i, j)[1]);
     }
     
-    
+    public void ver_metas(Regras reg, int ind_jog) {
+    	Jogo a = reg.novo;
+    	if(a.ver_pos(ind_jog, 8, 2)[1] > 1) {
+        	metas[0] = 1;
+        }
+    	if(a.ver_pos(ind_jog, 8, 8)[1] > 1) {
+        	metas[1] = 1;
+        }
+    	
+    }
     public void exibir(){
         for(String i[]: tab){
             for(String j: i){
