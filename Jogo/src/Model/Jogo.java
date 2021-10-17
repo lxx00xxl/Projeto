7f245b1986cad4f7effe71bcc6e9c4eb2679c409
caiovalle2 <caiovalle1999@hotmail.com>
2021-10-17 08:43:31 -0300

    Update Jogo.java

--- Jogo/src/Model/Jogo.java
+++ Jogo/src/Model/Jogo.java
@@ -180,7 +180,7 @@ class Jogo{
     	
         /*Se a posicao que o explorador for movimentar tiver no maximo 1 explorador do outro jogador (ou time se for modo 1) ou for o polo oposto*/
         if((ver_pos(ind_jog,dis[1],dis[0])[1] > -2    && capturar(ind_jog,dis[1],dis[0])) || (jog[ind_jog].polo_oposto[1] == dis[1] && jog[ind_jog].polo_oposto[0] == dis[0]) ){
-        	System.out.println("Movimentou!");
+        	
         	jog[ind_jog].setposicao(ind_exp, dis[1], dis[0]);
         	return 1;
         }        
