66fda4a05a3a7ca78015f2626fc984873d6e3a69
caiovalle2 <caiovalle1999@hotmail.com>
2021-10-17 08:27:18 -0300

    Update Main.java

--- Jogo/src/Teste/Main.java
+++ Jogo/src/Teste/Main.java
@@ -8,17 +8,36 @@ public class Main
 	public static void main(String[] args) {
 	
 	Scanner s = new Scanner(System.in);
-	int exp, pos,sentido, jog =0;
+	int exp, pos,sentido, jog =0, ant =0, qnt, modo = 0;
 	int dado[] = new int[2], pos_ant[],mov;
 	String tab[][] = new String[15][20];
-	Regras a = new Regras(0,4);
 	Tab tabuleiro = new Tab(tab);
-
-	while(!a.ganhou(jog)){
-	    System.out.println("Sua vez: Jogue o dado");
+	char cor;
+	System.out.println("Quantos jogadores: 2 ou 4");
+    qnt = s.nextInt();
+    if(qnt >2) {
+    	 System.out.println("modo: 0 - individual 1 - duplas");
+    	 modo = s.nextInt();
+    }
+    Regras a = new Regras(modo,qnt);
+	while(!a.ganhou(ant)){
+		exp = -1;
+	    System.out.println(jog+ " Sua vez: Jogue o dado");
 	    dado = a.jogardado();
 	    System.out.println("dado 0:"+dado[0]);
 	    System.out.println("dado 1:"+dado[1]);
+	    
+	    if(dado[0] == dado[1]) {
+	    	cor = a.dadocolorido();
+	    	System.out.println("Dado colorido!");
+	    	System.out.println("cor: " + cor);
+	    	if(cor != 'P') {
+	    		System.out.println("Escolha o explorador do jogador da cor: 0 - 6");
+			    exp = s.nextInt();
+	    	}
+	    	a.acao_dado_colorido(jog, cor, exp);
+	    }
+	    
 	    System.out.println("Escolha o explorador: 0 - 6");
 	    exp = s.nextInt();
 	    System.out.println("Escolha qual dado usar: 0 ou 1");
@@ -31,7 +50,11 @@ public class Main
 	        if(mov == 1) {
 	        	a.movimentar(jog,exp,pos,-sentido);
 	        }
-	        
+	        else if(mov == -1) {
+	        	System.out.println("Explorador chegou no Polo. Escolha outro explorador:");
+	    	    exp = s.nextInt();
+	        }
+
 	        sentido = s.nextInt();
 	        mov = a.movimentar(jog,exp,pos,sentido);
 	        
@@ -49,6 +72,10 @@ public class Main
 	        if(mov == 1) {
 	        	a.movimentar(jog,exp,(pos+1)%2,-sentido);
 	        }
+	        else if(mov == -1) {
+	        	System.out.println("Explorador chegou no Polo. Escolha outro explorador:");
+	    	    exp = s.nextInt();
+	        }
 	        
 	        sentido = s.nextInt();
 	        mov = a.movimentar(jog,exp,(pos+1)%2,sentido);
@@ -57,11 +84,13 @@ public class Main
 	    }
 	    tabuleiro.atualiza_mapa(a, jog, exp,pos_ant);
 	    tabuleiro.exibir();
+	    ant = jog;
 	    jog++;
-	    if(jog>3){
+	    if(jog>qnt){
 	        jog =0;
 	    }
 	}
+	System.out.println("O ganhador foi o " + a.ver_ganhador());
 
 }
 }
